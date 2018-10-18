89210e56a563e0965875bf7139a3f5029710d4a4
KuzuharaHiroyasu <43770961+KuzuharaHiroyasu@users.noreply.github.com>
2018-10-18 16:09:27 +0900

    コーディング完了

--- ApneaRowDataAnalysis/ApneaRowDataAnalysis.cpp
+++ ApneaRowDataAnalysis/ApneaRowDataAnalysis.cpp
@@ -8,37 +8,71 @@
 #include	<time.h>
 #include	<direct.h>
 #include	<string.h>
+#include	"getwav.h"
+#include	"apnea_param.h"
+
+#ifdef __cplusplus
+#define DLLEXPORT extern "C" __declspec(dllexport)
+#else
+#define DLLEXPORT __declspec(dllexport)
+#endif
 
 /*==============================================================================*/
 /*	define定義																	*/
 /*==============================================================================*/
-#define BUF_SIZE					128
+#define DATA_SIZE				200	// 10秒間、50msに1回データ取得した数
+#define BUF_SIZE				256
+#define RIREKI					3
 
 /*==============================================================================*/
 /*	グローバル変数																*/
 /*==============================================================================*/
 double	pdata[BUF_SIZE];
+double  raw_[DATA_SIZE];
+double	dc_[DATA_SIZE];
+double	movave_[DATA_SIZE];
+int		temp_int_buf0[BUF_SIZE];
 char    path_[17] = "./";
-int len = 128;
+
+int		len = 128;
+int		snore_ = SNORE_OFF;		// いびき
+
+static B	SnoreTime_[RIREKI];
+static UB	SnoreFlg_; // ONカウント中 or OFFカウント中
+static UB	SnoreCnt_; // ON連続回数, OFF連続回数 兼用
 
 /*==============================================================================*/
 /*	プロトタイプ宣言															*/
 /*==============================================================================*/
-void debug_out(char *f, const double d[], int size, const char* ppath, int no);
-void debug_out_int(char *f, const int d[], int size, const char* ppath, int no);
+void calc_snore_init(void);
+void getwav_snore(const double* pData, int DSize, double Param);
+static int proc_on(int Pos);
+static int proc_off(int Pos);
+static void Save(void);
+static void Reset(void);
+static void Judge(void);
+void	debug_out(char *f, const double d[], int size, const char* ppath);
+void	debug_out_int(char *f, const int d[], int size, const char* ppath);
 
 /*==============================================================================*/
 /*	main																		*/
 /*==============================================================================*/
 int main()
 {
-	FILE *fp;
 	struct tm timeptr;
+	FILE *fp;
+	double* ptest1 = NULL;
 	char folder[15] = { '\0' };
 	time_t timer = 0;
 	int i = 0;
 
-	if (fopen_s(&fp, "./raw.txt", "r")) {
+	ptest1 = (double*)calloc(len, sizeof(double));
+
+	if (!ptest1) {
+		return 1;
+	}
+
+	if (fopen_s(&fp, "./movave.txt", "r")) {
 		printf("ファイルを開くことが出来ませんでした。\n");
 		return 1;
 	}
@@ -56,24 +90,256 @@ int main()
 		_mkdir(path_);
 
 		for (i = 0; i < len; i++) {
-			fscanf_s(fp, "%lf", &(pdata[i]));     /*  1行読む  */
+			fscanf_s(fp, "%lf", &(movave_[i]));     /*  1行読む  */
 		}
 	}
+
+	for (int ii = 0; ii<len; ++ii) {
+		ptest1[ii] = movave_[ii] / APNEA_PARAM_RAW;
+	}
+
+	calc_snore_init();
+
+	getwav_snore(ptest1, len, APNEA_PARAM_SNORE);
+	
+	double tmpsnore = (double)snore_;
+	debug_out("snore_", &tmpsnore, 1, path_);
+
 	fclose(fp);
+}
+
+/************************************************************************/
+/* 関数     : calc_snore_init											*/
+/* 関数名   : 初期化処理												*/
+/* 引数     : なし														*/
+/* 戻り値   : なし														*/
+/* 変更履歴 : 2017.07.12 Axia Soft Design mmura	初版作成				*/
+/************************************************************************/
+/* 機能 :																*/
+/************************************************************************/
+/* 注意事項 :															*/
+/* なし																	*/
+/************************************************************************/
+void calc_snore_init(void)
+{
+	int ii;
+
+	SnoreFlg_ = OFF;
+	SnoreCnt_ = 0;
+	snore_ = SNORE_OFF;
+	for (ii = 0; ii<RIREKI; ++ii) {
+		SnoreTime_[ii] = -1;
+	}
+}
+
+/************************************************************************/
+/* 関数     : getwav_snore												*/
+/* 関数名   : いびき演算処理											*/
+/* 引数     : なし														*/
+/* 戻り値   : なし														*/
+/* 変更履歴 : 2017.07.12 Axia Soft Design mmura	初版作成				*/
+/************************************************************************/
+/* 機能 :																*/
+/************************************************************************/
+/* 注意事項 :															*/
+/* なし																	*/
+/************************************************************************/
+void getwav_snore(const double* pData, int DSize, double Param)
+{
+	int ii;
+	int jj;
+	int loop;
+	int size;
+	int pos = 0;
+
+	// 閾値を超えた回数の移動累計をとる
+	loop = DATA_SIZE_APNEA - SNORE_PARAM_SIZE;
+	for (ii = 0; ii<loop; ++ii) {
+		temp_int_buf0[ii] = 0;
+		size = ii + SNORE_PARAM_SIZE;
+		for (jj = ii; jj<size; ++jj) {
+			if (pData[jj] >= SNORE_PARAM_THRE) {
+				temp_int_buf0[ii] += 1;
+			}
+		}
+	}
+	debug_out_int("snore_Thre", temp_int_buf0, loop, path_);
+
+	while (pos < loop) {
+		switch (SnoreFlg_) {
+		case ON:
+			pos = proc_on(pos);
+			break;
+		case OFF:
+			pos = proc_off(pos);
+			break;
+		default:
+			calc_snore_init();
+			return;
+		}
+	}
+}
+
+/************************************************************************/
+/* 関数     : proc_on													*/
+/* 関数名   : いびきON時処理											*/
+/* 引数     : int Data : 波形データ										*/
+/* 戻り値   : なし														*/
+/* 変更履歴 : 2018.07.25 Axia Soft Design mmura	初版作成				*/
+/************************************************************************/
+/* 機能 :																*/
+/************************************************************************/
+/* 注意事項 :															*/
+/* なし																	*/
+/************************************************************************/
+static int proc_on(int Pos)
+{
+	int ii;
+	int loop = DATA_SIZE_APNEA - SNORE_PARAM_SIZE;
+	int pos = loop;
+
+	// OFF確定している場所を特定する
+	for (ii = Pos; ii<loop; ++ii) {
+		if (temp_int_buf0[ii] <= SNORE_PARAM_OFF_CNT) {
+			SnoreFlg_ = OFF;
+			pos = ii;
+			Save();
+			Judge();
+			break;
+		}
+		else {
+			SnoreCnt_ += 1;
+		}
+	}
+
+	return pos;
+}
+
+/************************************************************************/
+/* 関数     : proc_off													*/
+/* 関数名   : いびきOFF時処理											*/
+/* 引数     : int Data : 波形データ										*/
+/* 戻り値   : なし														*/
+/* 変更履歴 : 2018.07.25 Axia Soft Design mmura	初版作成				*/
+/************************************************************************/
+/* 機能 :																*/
+/************************************************************************/
+/* 注意事項 :															*/
+/* なし																	*/
+/************************************************************************/
+static int proc_off(int Pos)
+{
+	int ii;
+	int loop = DATA_SIZE_APNEA - SNORE_PARAM_SIZE;
+	int pos = loop;
+
+	// ON確定している場所を特定する
+	for (ii = Pos; ii<loop; ++ii) {
+		if (temp_int_buf0[ii] >= SNORE_PARAM_ON_CNT) {
+			SnoreFlg_ = ON;
+			SnoreCnt_ = 0;
+			pos = ii;
+			break;
+		}
+		else {
+			SnoreCnt_ += 1;
+			if (SnoreCnt_ >= SNORE_PARAM_NORMAL_CNT) {
+				Reset();
+			}
+		}
+	}
+
+	return pos;
+}
+
+/************************************************************************/
+/* 関数     : Save														*/
+/* 関数名   : いびき時間を保存											*/
+/* 引数     : なし														*/
+/* 戻り値   : なし														*/
+/* 変更履歴 : 2018.07.25 Axia Soft Design mmura	初版作成				*/
+/************************************************************************/
+/* 機能 :																*/
+/************************************************************************/
+/* 注意事項 :															*/
+/* なし																	*/
+/************************************************************************/
+static void Save(void)
+{
+	int ii;
+
+	for (ii = 1; ii<RIREKI; ++ii) {
+		SnoreTime_[RIREKI - ii] = SnoreTime_[RIREKI - ii - 1];
+	}
+	SnoreTime_[0] = SnoreCnt_;
+	SnoreCnt_ = 0;
+}
+
+/************************************************************************/
+/* 関数     : Judge														*/
+/* 関数名   : いびき判定												*/
+/* 引数     : なし														*/
+/* 戻り値   : なし														*/
+/* 変更履歴 : 2018.07.25 Axia Soft Design mmura	初版作成				*/
+/************************************************************************/
+/* 機能 :																*/
+/************************************************************************/
+/* 注意事項 :															*/
+/* なし																	*/
+/************************************************************************/
+static void Judge(void)
+{
+	int ii;
+
+	for (ii = 0; ii<RIREKI; ++ii) {
+		if (SnoreTime_[ii] == -1) {
+			return;
+		}
+	}
+
+	for (ii = 0; ii<RIREKI - 1; ++ii) {
+		if (abs(SnoreTime_[0] - SnoreTime_[ii + 1]) > SNORE_PARAM_GOSA) {
+			return;
+		}
+	}
+	snore_ = SNORE_ON;
+}
+
+/************************************************************************/
+/* 関数     : Reset														*/
+/* 関数名   : 通常呼吸への復帰処理										*/
+/* 引数     : なし														*/
+/* 戻り値   : なし														*/
+/* 変更履歴 : 2018.07.25 Axia Soft Design mmura	初版作成				*/
+/************************************************************************/
+/* 機能 :																*/
+/************************************************************************/
+/* 注意事項 :															*/
+/* なし																	*/
+/************************************************************************/
+static void Reset(void)
+{
+	int ii;
+
+	for (ii = 0; ii<RIREKI; ++ii) {
+		SnoreTime_[ii] = -1;
+	}
+	snore_ = SNORE_OFF;
 }
 
 /*==============================================================================*/
 /*	debug_out																	*/
 /*==============================================================================*/
-void	debug_out(char *f, const double d[], int size, const char* ppath, int no)
+void	debug_out(char *f, const double d[], int size, const char* ppath)
 {
 	FILE		*fp;
 	char		b[1024];
+	errno_t error;
 
-	sprintf_s(b, sizeof(b), "%s\\%s(%d).txt", ppath, f, no);
+	sprintf_s(b, 1024, "%s\\%s.txt", ppath, f);
 
-
-	if (fopen_s(&fp, b, "w"))
+	error = fopen_s(&fp, b, "w");
+	if (error != 0)
 	{
 		printf("file open error [debug_out]\n");
 		exit(0);
@@ -89,12 +355,12 @@ void	debug_out(char *f, const double d[], int size, const char* ppath, int no)
 /*==============================================================================*/
 /*	debug_out_int																*/
 /*==============================================================================*/
-void	debug_out_int(char *f, const int d[], int size, const char* ppath, int no)
+void	debug_out_int(char *f, const int d[], int size, const char* ppath)
 {
 	FILE		*fp;
 	char		b[1024];
 
-	sprintf_s(b, sizeof(b), "%s\\%s(%d).txt", ppath, f, no);
+	sprintf_s(b, sizeof(b), "%s\\%s.txt", ppath, f);
 
 
 	if (fopen_s(&fp, b, "w"))
---
+++ ApneaRowDataAnalysis/getwav.h
@@ -0,0 +1,74 @@
+/************************************************************************/
+/* システム名   : 無呼吸判定											*/
+/* ファイル名   : getwav.h												*/
+/* 機能         : inputファイルの読み込み								*/
+/* 変更履歴     : 2017.07.12 Axia Soft Design mmura	初版作成			*/
+/* 注意事項     : なし                                                  */
+/************************************************************************/
+
+#ifdef __cplusplus
+#define DLLEXPORT extern "C" __declspec(dllexport)
+#else
+#define DLLEXPORT __declspec(dllexport)
+#endif
+
+#ifndef		_GETWAV_H_			/* 二重定義防止 */
+#define		_GETWAV_H_
+
+#include "sys.h"
+
+
+/************************************************************/
+/* マクロ													*/
+/************************************************************/
+#define DATA_SIZE_APNEA		(200)
+#define SNORE_PARAM_SIZE	(10)
+#define SNORE_PARAM_THRE	(256)
+
+// 無呼吸判定結果
+#define APNEA_NORMAL	0	// 異常なし
+#define APNEA_WARN		1	// 無呼吸区間あり
+#define APNEA_ERROR		2	// 完全無呼吸
+#define APNEA_NONE		3	// 判定エラー
+
+// いびき判定結果
+#define SNORE_OFF		0	// いびきなし
+#define SNORE_ON		1	// いびきあり
+
+// OFF判定回数
+#define SNORE_PARAM_OFF_CNT				(0)
+// ON判定回数
+#define SNORE_PARAM_ON_CNT				(8)
+// いびきあり -> なしへの判定回数
+#define SNORE_PARAM_NORMAL_CNT			(290)
+// 許容誤差 0.4s
+#define SNORE_PARAM_GOSA				(8)
+
+/************************************************************/
+/* 型定義													*/
+/************************************************************/
+
+/************************************************************/
+/* 外部参照宣言												*/
+/************************************************************/
+// 演算実行
+DLLEXPORT void __stdcall getwav_init(int* data, int len, char* ppath, int* psnore);
+// DC成分除去データ
+DLLEXPORT void __stdcall getwav_dc(double* pdata);
+
+// 無呼吸演算
+DLLEXPORT void __stdcall get_apnea_ave(double* pdata);
+DLLEXPORT void __stdcall get_apnea_eval(int* pdata);
+DLLEXPORT void __stdcall get_apnea_rms(double* pdata);
+DLLEXPORT void __stdcall get_apnea_point(double* pdata);
+// いびき演算
+DLLEXPORT void __stdcall get_snore_xy2(double* pdata);
+DLLEXPORT void __stdcall get_snore_interval(double* pdata);
+// 結果
+DLLEXPORT int  __stdcall get_state(void);
+#endif
+
+/************************************************************/
+/* END OF TEXT												*/
+/************************************************************/
+
